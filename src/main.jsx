92fbaffb8cb450fe593047e78f2d26088e8c030a
Prathamesh Balkrishna Shinde <109452236+Patushinde@users.noreply.github.com>
2025-12-07 17:18:16 +0530

    CHANEGS

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 )
